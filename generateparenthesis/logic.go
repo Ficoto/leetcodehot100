a82793f452d64f5bc30e7566b32cae4e9010f7f0
zhengfucheng <438500829@qq.com>
2022-07-14 17:06:20 +0800

    generate parenthesis

--- generateparenthesis/logic.go
+++ generateparenthesis/logic.go
@@ -1,1 +1,36 @@
-package generateparenthesis
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(generateParenthesis(3))
+}
+
+var cache = [100][]string{}
+
+// https://leetcode.cn/problems/generate-parentheses/
+func generateParenthesis(n int) []string {
+	generate(n)
+	return cache[n]
+}
+
+func generate(n int) {
+	if cache[n] != nil {
+		return
+	}
+	if n == 0 {
+		cache[0] = []string{""}
+		return
+	}
+	var res []string
+	for i := 0; i != n; i++ {
+		generate(i)
+		generate(n - i - 1)
+		for _, left := range cache[i] {
+			for _, right := range cache[n-i-1] {
+				res = append(res, "("+left+")"+right)
+			}
+		}
+	}
+	cache[n] = res
+}
